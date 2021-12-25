875ddc7ea247b1defe2fb8a70311a017a8f42f50
FlutLab.io <>
2021-12-25 13:49:39 +0000

    Initial commit

---
+++ lib/servicios/sunat_service.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:nava/modelo/cls_Tipo_Cambio.dart';
+
+class SunatService {
+  Future<TipoCambio> getTipoCambio(String fechaAsString) async {
+    try {
+      http.Response response = (await http.get('https://api.sunat.cloud/cambio/$fechaAsString'));
+      if (response.statusCode != 200) {
+        // tirar excepcion
+      }
+      print('response.body: ${response.body}');
+      var data = jsonDecode(response.body);
+      print('data en el service $data');
+      TipoCambio tc = new TipoCambio(fecha: fechaAsString, compra: data[fechaAsString]['compra'], venta: data[fechaAsString]['venta']);
+      return tc;
+    } catch (e) {
+      throw 'No hay TC para la fecha seleccionada';
+    }
+  }
+}
---
+++ lib/vista/pagina_inicio.dart
@@ -0,0 +1,79 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:nava/componentes/mi_cls_boton.dart';
+import 'package:nava/vista/pagina_Tipo_Cambio.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('TuCambioPe')),
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Hero(
+                tag: 'dollar',
+                child: Image.asset(
+                  'imgs/dollar.png',
+                  scale: 0.5,
+                ),
+              ),
+              SizedBox(
+                height: 36,
+                width: double.infinity,
+              ),
+              SizedBox(
+                width: 250.0,
+                child: ScaleAnimatedTextKit(
+                    repeatForever: true,
+                    scalingFactor: 2,
+                    onTap: () {
+                      print("Tap Event");
+                    },
+                    text: [
+                      "Bienvenidos a TuCambioPe",
+                      "Soles / Dolares"
+                    ],
+                    textStyle: TextStyle(
+                      fontSize: 24.0,
+                      fontFamily: "Canterbury",
+                      color: Colors.black45,
+                    ),
+                    textAlign: TextAlign.center,
+                    alignment: AlignmentDirectional.topStart // or Alignment.topLeft
+                    ),
+              ),
+              /*
+              Text(
+                'Bienvenidos a \n TuCambioPe!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),*/
+              SizedBox(
+                height: 36,
+                width: double.infinity,
+              ),
+              MiBoton(
+                texto: 'Ingresar',
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TipoCambioScreen(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
